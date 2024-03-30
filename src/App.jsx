e4fee98e369997fcb2c80dbcd5fbba5819308f17
aldrinj0se <aldrinsilvam@gmail.com>
2024-03-30 02:31:38 -0300

    Entrega Final FEIII - Aldrin Silva 30/03/24 2:31

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-
-import Footer from "./Components/Footer";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-
+import Footer from "./Components/Footer";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Favs from "./Routes/Favs";
+import Detail from "./Routes/Detail";
+import { useContextGlobal } from "./Components/utils/Global.context";
 
 function App() {
+  const { toggleTheme } = useContextGlobal();
+
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <div className={`${toggleTheme} indexApp`}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/favs" element={<Favs />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="*" element={<h1>¡Ruta no encontrada!</h1>} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
